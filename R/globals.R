7c790bf5a8891297ab14e85ba0b0036339a87db7
Sangwan <sangwan0923@gmail.com>
2025-12-05 07:13:19 -0500

    fixed errata

--- R/globals.R
+++ R/globals.R
@@ -1,8 +1,12 @@
 # R/globals.R
 
 #' Global variables used in szKendall
-#' 
+#'
+#' @name szKendall-globals
 #' @keywords internal
+#' @noRd
+NULL
+
 utils::globalVariables(c(
-    "foreach","%dopar%","para"
+  "foreach", "%dopar%", "para"
 ))
