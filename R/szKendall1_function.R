348a8f5603368edbae0a1346d4d7273bfbff9d6a
Sangwan <sangwan0923@gmail.com>
2025-12-05 09:05:06 -0500

    fixed szkendall1.dist check order

--- R/szKendall1_function.R
+++ R/szKendall1_function.R
@@ -45,13 +45,15 @@ szKendall1.diss <- function(mat_count,kendall.dist,...){
     szkendall1.dist[scoreS1[m,1], scoreS1[m,2]] <- scoreS1[m,3]
   }
 
-  if(any(szkendall1.dist[upper.tri(szkendall1.dist,diag=F)] < 1e-5)) {
-    warning("Parallel computing of szKendall among the cell pairs is not done properly. Please re-run the function and/or re-set the parallel backend.")
-  }
+  
 
   szkendall1.dist <- (szkendall1.dist + t(szkendall1.dist)) # symmetrize
 
   szkendall1.dist <- (szkendall1.dist + kendall.dist)      # add Kendall part
+  
+  if(any(szkendall1.dist[upper.tri(szkendall1.dist,diag=F)] < 1e-5)) {
+    warning("Parallel computing of szKendall among the cell pairs is not done properly. Please re-run the function and/or re-set the parallel backend.")
+  }
 
   rownames(szkendall1.dist) <- NULL
   colnames(szkendall1.dist) <- NULL
